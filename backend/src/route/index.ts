d4f906d4f012553c6f0bd25d4e123ad423a14cfe
Charles Nollet <chrlsnllt@gmail.com>
2019-01-19 18:35:16 -0500

    functional backend

--- backend/src/route/index.ts
+++ backend/src/route/index.ts
@@ -1,11 +1,17 @@
 import * as express from 'express';
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://hack:Hackatown2019@ds159574.mlab.com:59574/hackdb', {useNewUrlParser: true});
+
+const Game = mongoose.model('Game', { name: String });
+
 export class Index {
     public router = express.Router();
 
     constructor() {
         this.router.get('/', this.index.bind(this));
-        this.router.get('/test', this.test.bind(this));
+        this.router.post('/test', this.test.bind(this));
+        this.router.get('/games', this.getAllGames.bind(this));
     }
 
     private index(req: express.Request, res: express.Response) {
@@ -14,7 +20,13 @@ export class Index {
         });
     }
 
-    private test(req: express.Request, res: express.Response) {
-        res.send("test");
+    private async test(req: express.Request, res: express.Response) {
+        const game = new Game(req.body);
+        await game.save();
+        res.end();
+    }
+
+    private async getAllGames(req: express.Request, res: express.Response) {
+        res.send(await Game.find());
     }
 }
