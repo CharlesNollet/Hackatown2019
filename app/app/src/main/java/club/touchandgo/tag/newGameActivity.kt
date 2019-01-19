f96d76ec5c1eed847ba4fd58d8b4610f16dbffd7
Samuel Ouvrard <samuelroyouvrard@gmail.com>
2019-01-19 14:57:29 -0500

    ajout de mainActivity et newGameActivity

---
+++ app/app/src/main/java/club/touchandgo/tag/newGameActivity.kt
@@ -0,0 +1,12 @@
+package club.touchandgo.tag
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class newGameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_game)
+    }
+}
