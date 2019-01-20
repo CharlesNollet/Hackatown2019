580327991659c448b0830bdf5b8c6cdd2f7a621a
Charles Nollet <chrlsnllt@gmail.com>
2019-01-19 21:17:30 -0500

    Continution du testing de post sur serveur

--- app/app/src/main/java/club/touchandgo/tag/test.kt
+++ app/app/src/main/java/club/touchandgo/tag/test.kt
@@ -2,30 +2,51 @@ package club.touchandgo.tag
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.github.kittinunf.fuel.*
 import com.github.kittinunf.result.*
 import kotlinx.android.synthetic.main.activity_test.*
+import org.json.JSONObject
 
 class test : AppCompatActivity() {
 
-    var URL = "http://207.246.122.125:8080/games"
+    var postPlayer = "http://207.246.122.125:8080/postPlayer"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
+    }
 
+    override fun onResume(){
+        super.onResume()
+        val json = JSONObject()
+        json.put("username", "user1")
+        json.put("lat", "112")
+        json.put("long", "74")
+        json.put("tag", "false")
 
-            URL.httpGet()
-            .responseString { request, response, result ->
-                when (result) {
-                    is Result.Failure -> {
-                        val ex = result.error.exception
-                    }
-                    is Result.Success -> {
-                        val data = result.get()
-                        getResult.text = data
-                    }
+        postPlayer.httpPost().body(json.toString()).responseString{ _, _, result ->
+            when (result) {
+                is Result.Failure -> {
+                    val ex = result.error.exception
+                    Log.e("Error", ex.toString())
+                }
+                is Result.Success -> {
+
                 }
             }
+        }
+
+//        URL.httpGet().responseString { request, response, result ->
+//            when (result) {
+//                is Result.Failure -> {
+//                    val ex = result.error.exception
+//                }
+//                is Result.Success -> {
+//                    val data = result.get()
+//                    getResult.text = data
+//                }
+//            }
+//        }
     }
 }
--- backend/src/route/index.ts
+++ backend/src/route/index.ts
@@ -3,15 +3,19 @@ import * as express from 'express';
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://hack:Hackatown2019@ds159574.mlab.com:59574/hackdb', {useNewUrlParser: true});
 
-const Game = mongoose.model('Game', { name: String });
+// const Game = mongoose.model('Game', { name: String });
+const Player = mongoose.model('Player', {username: String, lat : Number, long: Number, tag: Boolean})
 
 export class Index {
     public router = express.Router();
 
     constructor() {
         this.router.get('/', this.index.bind(this));
-        this.router.post('/postgame', this.postGame.bind(this));
-        this.router.get('/games', this.getAllGames.bind(this));
+        // this.router.post('/postgame', this.postGame.bind(this));
+        // this.router.get('/games', this.getAllGames.bind(this));
+        this.router.get('/getPlayer', this.getPlayers.bind(this));
+        this.router.delete('/deletePlayer', this.deletePlayer.bind(this));
+        this.router.post('/postPlayer', this.postPlayer.bind(this));
     }
 
     private index(req: express.Request, res: express.Response) {
@@ -20,13 +24,27 @@ export class Index {
         });
     }
 
-    private async postGame(req: express.Request, res: express.Response) {
-        const game = new Game(req.body);
-        await game.save();
-        res.end();
+    // private async postGame(req: express.Request, res: express.Response) {
+    //     const game = new Game(req.body);
+    //     await game.save();
+    //     res.end();
+    // }
+
+    // private async getAllGames(req: express.Request, res: express.Response) {
+    //     res.send(await Game.find());
+    // }
+
+    private async getPlayers(req: express.Request, res:express.Response) {
+        res.send(await Player.find());
     }
 
-    private async getAllGames(req: express.Request, res: express.Response) {
-        res.send(await Game.find());
+    private async deletePlayer(req: express.Request, res:express.Response) {
+        await this.getPlayers(req, res);
+    }
+
+    private async postPlayer(req:express.Request, res:express.Response) {
+        const player = new Player(req.body);
+        await player.save();
+        res.end();
     }
 }
